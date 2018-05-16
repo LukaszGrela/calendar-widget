d994821c8f98dd2c4f5fa7229dbf395b9ac6d9a0
Łukasz 'Severiaan' Grela <lukasz.grela@gmail.com>
2018-05-16 22:42:29 +0100

    Fixed linting issues. Removed anonymous function from the render.

--- src/components/CalendarDay.js
+++ src/components/CalendarDay.js
@@ -4,20 +4,23 @@ import { momentObj } from 'react-moment-proptypes';
 import { noop } from '../utils/helpers';
 
 class CalendarDay extends React.Component {
+    clickHandler = () => {
+        const { date, handleClick = noop } = this.props;
+        handleClick(date);
+    }
     render = () => {
-        const { date, className, handleClick = noop } = this.props;
+        const { date, className } = this.props;
 
         const text = typeof date === 'string' ? date : date.date();
         return (
             <div className={'day' + (className ? ' ' + className : '')}
-                onClick={
-                    () => { handleClick(date) }
-                }
+                onClick={this.clickHandler}
             >{text}</div>
-        )
+        );
     }
 }
 CalendarDay.propTypes = {
+    className: PropTypes.string,
     date: PropTypes.oneOfType([PropTypes.string, momentObj]).isRequired,
     handleClick: PropTypes.func
 };
