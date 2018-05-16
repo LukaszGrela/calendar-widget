25a77701135835ee5d944dbb7e4897bf7a3c3a9f
Łukasz 'Severiaan' Grela <lukasz.grela@gmail.com>
2018-05-16 22:35:58 +0100

    Removed unused inmport, version bump.

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 import Page404 from '../pages/Page404';
 import Home from '../pages/Home';
-import moment from 'moment';
 import RouterCalendar from '../pages/RouterCalendar';
 import LinkedCalendars from '../pages/LinkedCalendars';
 
@@ -12,15 +11,15 @@ const AppRouter = () => (
             <header><h1>Calendar Widget</h1></header>
             <Switch>
                 <Route exact path="/index.html" component={() => {
-                    return <Redirect to='/' />
+                    return <Redirect to='/' />;
                 }} />
-            {/*
+                {/*
                 <Route exact path="/index.html" component={() => {
                     const now = moment();
                     return <Redirect to={`/${now.year()}/${now.month()}/${now.date()}`} />
                 }} />
             */}
-                <Route path="/" exact component={Home} />    
+                <Route path="/" exact component={Home} />
                 <Route path="/router-calendar/:year?/:month?/:date?" component={RouterCalendar} />
                 <Route path="/linked-calendars" exact component={LinkedCalendars} />
                 <Route component={Page404} />
