abbf9f7d5aec362711535097a86ec0a65cf30eed
Łukasz 'Severiaan' Grela <lukasz.grela@gmail.com>
2018-05-16 22:46:09 +0100

    Fix linting issues. Removed anonymous function from the render.

--- src/components/CalendarMonthGrid.js
+++ src/components/CalendarMonthGrid.js
@@ -1,26 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { momentObj } from 'react-moment-proptypes';
-import moment from 'moment';
 import CalendarRow from './CalendarRow';
 import { calendarDates, noop } from '../utils/helpers';
 
 class CalendarMonthGrid extends React.Component {
-    render() {
+
+    renderCalendarWeeks = () => {
         const { now, date, dayClicked = noop } = this.props;
         const weeks = calendarDates(date);
+        weeks.map((week, index) => (
+            <CalendarRow
+                key={index}
+                className={`week row-${index}`}
+                days={week}
+                now={now}
+                current={date}
+                dayClicked={dayClicked} />
+        ));
+    }
+
+    render() {
         return (
             <div className='month-grid'>
                 {
-                    weeks.map((week, index) => {
-                        return <CalendarRow
-                            key={index}
-                            className={`week row-${index}`}
-                            days={week}
-                            now={now}
-                            current={date}
-                            dayClicked={dayClicked} />
-                    })
+                    this.renderCalendarWeeks()
                 }
             </div>
         );
@@ -30,6 +34,6 @@ CalendarMonthGrid.propTypes = {
     now: momentObj.isRequired,
     date: momentObj.isRequired,
     dayClicked: PropTypes.func
-}
+};
 
 export default CalendarMonthGrid;
