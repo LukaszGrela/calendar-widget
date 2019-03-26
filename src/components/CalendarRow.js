cd404df91b821d6d4a7dbf726444fdd39c6f9bd6
Lukasz Grela <lukasz.grela@gmail.com>
2019-03-26 13:07:01 +0000

    another dumb component

--- src/components/CalendarRow.js
+++ src/components/CalendarRow.js
@@ -4,47 +4,47 @@ import CalendarDay from './CalendarDay';
 import { momentObj } from 'react-moment-proptypes';
 import { noop } from '../utils/helpers';
 
-class CalendarRow extends React.Component {
+const CalendarRow = props => {
+  const renderCalendarDays = () => {
+    const { days, now, current, dayClicked = noop } = props;
+    return days.map((day, index) => {
+      const spill =
+        typeof date === 'string' || !current
+          ? ''
+          : day.month() !== current.month()
+          ? ' spill'
+          : '';
+      const today =
+        typeof date === 'string' || !now
+          ? ''
+          : day.month() === now.month() &&
+            day.date() === now.date() &&
+            day.year() === now.year()
+          ? ' today'
+          : '';
+      return (
+        <CalendarDay
+          key={day}
+          className={`day-${index}${spill}${today}`}
+          date={day}
+          handleClick={dayClicked}
+        />
+      );
+    });
+  };
 
-    renderCalendarDays = () => {
-        const { days, now, current, dayClicked = noop } = this.props;
-        return days.map((day, index) => {
-
-            const spill = typeof date === 'string' || !current ? '' :
-                (day.month() !== current.month() ? ' spill' : '');
-            const today = typeof date === 'string' || !now ? '' :
-                (day.month() === now.month() &&
-                    day.date() === now.date() &&
-                    day.year() === now.year() ? ' today' : '');
-
-            return (
-                <CalendarDay
-                    key={day}
-                    className={`day-${index}${spill}${today}`}
-                    date={day}
-                    handleClick={dayClicked}
-                />
-            );
-        });
-    }
-
-    render = () => {
-        const { className } = this.props;
-        return (
-            <div className={'row' + (className ? ' ' + className : '')}>
-                {
-                    this.renderCalendarDays()
-                }
-            </div>
-        );
-    }
-}
+  return (
+    <div className={'row' + (props.className ? ' ' + props.className : '')}>
+      {renderCalendarDays()}
+    </div>
+  );
+};
 CalendarRow.propTypes = {
-    days: PropTypes.array.isRequired,
-    now: momentObj,
-    current: momentObj,
-    dayClicked: PropTypes.func,
-    className: PropTypes.string
+  days: PropTypes.array.isRequired,
+  now: momentObj,
+  current: momentObj,
+  dayClicked: PropTypes.func,
+  className: PropTypes.string,
 };
 
 export default CalendarRow;
