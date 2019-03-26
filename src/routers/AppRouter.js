a4abb3d822fd9e0a85de44a85068d2f5c5cda4c5
Lukasz Grela <lukasz.grela@gmail.com>
2019-03-26 14:07:13 +0000

    update path and added basename to BrowserRouter

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -6,31 +6,40 @@ import RouterCalendar from '../pages/RouterCalendar';
 import LinkedCalendars from '../pages/LinkedCalendars';
 
 const AppRouter = () => (
-    <BrowserRouter>
-        <div>
-            <header><h1>Calendar Widget</h1></header>
-            <Switch>
-                <Route exact path="/index.html" component={() => {
-                    return <Redirect to='/' />;
-                }} />
-                {/*
+  <BrowserRouter basename="/react/calendar-widget">
+    <div>
+      <header>
+        <h1>Calendar Widget</h1>
+      </header>
+      <Switch>
+        <Route
+          exact
+          path="/index.html"
+          component={() => {
+            return <Redirect to="/" />;
+          }}
+        />
+        {/*
                 <Route exact path="/index.html" component={() => {
                     const now = moment();
                     return <Redirect to={`/${now.year()}/${now.month()}/${now.date()}`} />
                 }} />
             */}
-                <Route path="/" exact component={Home} />
-                <Route path="/router-calendar/:year?/:month?/:date?" component={RouterCalendar} />
-                <Route path="/linked-calendars" exact component={LinkedCalendars} />
-                <Route component={Page404} />
-            </Switch>
-            <footer>
-                <div className='credits'>
-                    Arrow Up/Down by Design Effectz from the Noun Project
-                </div>
-            </footer>
+        <Route path="/" exact component={Home} />
+        <Route
+          path="/router-calendar/:year?/:month?/:date?"
+          component={RouterCalendar}
+        />
+        <Route path="/linked-calendars" exact component={LinkedCalendars} />
+        <Route component={Page404} />
+      </Switch>
+      <footer>
+        <div className="credits">
+          Arrow Up/Down by Design Effectz from the Noun Project
         </div>
-    </BrowserRouter>
+      </footer>
+    </div>
+  </BrowserRouter>
 );
 
 export default AppRouter;
